02f28f363b6ac569d1b16b70dd52fc0cf119f5d1
Johan <suarezjohan740@gmail.com>
2026-03-04 02:51:38 -0500

    feat: initial project with ink and typescript

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'ink'
+
+function App() {
+    return <Text color="green">gitproblem</Text>
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import { render } from "ink";
+import App from "./app"; 
+
+render(<App />)
